93c40ae8ce1c355f52f10f3fba2a7bc54c6677c6
nick <nickelias57@yahoo.com>
2021-03-19 22:19:19 -0500

    Initial Commit

---
+++ StockChecker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockChecker
+//
+//  Created by Nick Elias on 3/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockChecker/StockCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockCheckerApp.swift
+//  StockChecker
+//
+//  Created by Nick Elias on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct StockCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
